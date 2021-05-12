ed0598ea9c9eed21f7cf1ce2cf54e1c526e7be0a
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-05-12 20:35:19 +0300

    initial-commit

--- tribune/settings.py
+++ tribune/settings.py
@@ -80,15 +80,34 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tribune',
-        'USER': 'natasha',
-        'PASSWORD':'natasha',
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+#development
+if config('MODE')=="dev":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('tribune'),
+            'USER': config('natasha'),
+            'PASSWORD': config('natasha'),
+            'HOST': config('127.0.0.1'),
+           'PORT': '',
+        }
     }
-}
+
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Password validation
