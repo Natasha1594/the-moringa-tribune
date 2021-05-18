dd01ce5c6e353a5bbc7c77e43c0e18c7f5fc1a36
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-05-18 12:09:51 +0300

    update

--- news/views.py
+++ news/views.py
@@ -1,57 +1,61 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse, Http404
-import datetime as dt
-from .models import Article
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from .models import Article,NewsLetterRecipients
 from .forms import NewsLetterForm
-
+import datetime as dt
+from .email import send_welcome_email
 # Create your views here.
+#def welcome(request):
+    #return render(request, 'welcome.html')
 def news_today(request):
-    date = dt.date.today()
-    news = Article.todays_news()
+    date= dt.date.today()
+    news= Article.todays_news()
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+            #HttpResponseRedirect('news_today')
+           
+            
     else:
-        form = NewsLetterForm()
+            form = NewsLetterForm()
     return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
-
-
-
-def past_days_news(request,past_date):
-    
+def convert_dates(dates):
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+# View Function to present news from past days
+def past_days_news(request, past_date):
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
         assert False
-
     if date == dt.date.today():
         return redirect(news_today)
-    
     news = Article.days_news(date)
-    return render(request, 'all-news/past-news.html', {"date":date,"news":news})
-    
-
+    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
 def search_results(request):
-
     if 'article' in request.GET and request.GET["article"]:
         search_term = request.GET.get("article")
         searched_articles = Article.search_by_title(search_term)
         message = f"{search_term}"
-
         return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
-
     else:
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html',{"message":message})
-
-
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
-    except DoesNotExist:
+    except Article.DoesNotExist:
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
--- tribune/settings.py
+++ tribune/settings.py
@@ -15,6 +15,12 @@ import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,6 +38,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
