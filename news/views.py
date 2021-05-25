d591997fb2d6fe23f61194dd0c1f1a295cf7cf18
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-05-25 06:57:12 +0300

    updated views.py file

--- news/views.py
+++ news/views.py
@@ -6,6 +6,10 @@ import datetime as dt
 from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  MoringaMerch
+from .serializer import MerchSerializer
 
 def news_today(request):
     date = dt.date.today()
@@ -94,3 +98,9 @@ def newsletter(request):
     send_welcome_email(name, email)
     data = {'success': 'You have been successfully added to mailing list'}
     return JsonResponse(data)
+
+class MerchList(APIView):
+    def get(self, request, format=None):
+        all_merch = MoringaMerch.objects.all()
+        serializers = MerchSerializer(all_merch, many=True)
+        return Response(serializers.data)
